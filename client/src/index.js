ad096fb635dfc76b46f8ed9bf78c0ea83b9fceaa
Aslan Eminovi <mac@AslantekiMacBook-Pro.local>
2025-03-04 23:37:15 +0300

    Fix build error: Add react-bootstrap dependency and import Bootstrap CSS

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import './i18n/config';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import "./i18n/config";
+// Import Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <App />
