3247a7ff6dc7889dfecea2928b8e2215894fbd5f
AslanEminovi <aslan.eminovi@istanbulticaret.edu.tr>
2025-04-18 17:03:59 +0300

    Update StatsCards component to trigger deployment

--- client/src/components/StatsCards.jsx
+++ client/src/components/StatsCards.jsx
@@ -1,3 +1,4 @@
+// StatsCards component - Updated to trigger deployment
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExchangeAlt, faSpinner, faCheckCircle, faDollarSign } from '@fortawesome/free-solid-svg-icons';
